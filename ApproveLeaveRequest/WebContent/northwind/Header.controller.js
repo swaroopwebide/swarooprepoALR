69352c3f9da3cd210db679d53c75b74b8fd56698
swaroopwebide <forsapwebide1@gmail.com>
2017-07-12 22:21:58 -0700

    Revert "Add files via upload"

--- ApproveLeaveRequest/WebContent/northwind/Header.controller.js
+++
@@ -1,8 +0,0 @@
-sap.ui.controller("northwind.Header", {
-
-	onInit: function() {
-
-		//var oModel = new sap.ui.model.odata.ODataModel("https://cors-anywhere.herokuapp.com/services.odata.org/Northwind/Northwind.svc/", false);
-		
-	}
-});
